6e06a15feae35d4c1bf8662a7c5c1b3b1e4cf899
yongxu16 <yongxu16@163.com>
2016-02-23 17:56:45 +0800

    modify ini java

--- src/test/java/ini/IniTest.java
+++ src/test/java/ini/IniTest.java
@@ -2,7 +2,14 @@ package ini;
 
 import org.apache.shiro.web.env.EnvironmentLoaderListener;
 import org.apache.shiro.web.servlet.ShiroFilter;
+import org.apache.shiro.web.tags.AuthenticatedTag;
 import org.apache.shiro.web.tags.GuestTag;
+import org.apache.shiro.web.tags.HasAnyRolesTag;
+import org.apache.shiro.web.tags.HasPermissionTag;
+import org.apache.shiro.web.tags.HasRoleTag;
+import org.apache.shiro.web.tags.LacksPermissionTag;
+import org.apache.shiro.web.tags.LacksRoleTag;
+import org.apache.shiro.web.tags.PrincipalTag;
 import org.apache.shiro.web.tags.UserTag;
 
 public class IniTest {
@@ -12,5 +19,12 @@ public class IniTest {
 		ShiroFilter s ;
 		UserTag u ;
 		GuestTag g ; 
+		AuthenticatedTag at ;
+		PrincipalTag pt ;
+		HasRoleTag hrt ;
+		LacksRoleTag lrt ;
+		HasAnyRolesTag hart;
+		HasPermissionTag hpt ;
+		LacksPermissionTag lpt ;
 	}
 }
